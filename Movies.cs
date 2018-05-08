b74b926879e0f939284ce538c9669b9159e86b3c
Jasmine Hollis <jasmine.hollis@rocketmail.com>
2018-05-08 09:33:29 -0400

    First Commit of Lab 11

---
+++ Movies.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11_Movie
+{
+    class Movies
+    {
+        #region Data Members
+        private string title;
+        private string category;
+        #endregion
+
+        #region Properties
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+        #endregion
+
+
+        #region Default constructor - Allows us to initialize the object
+        public Movies()
+        {
+
+            title = "Movie Title";
+
+        }
+        #endregion
+
+        #region Overloaded constructors
+        public Movies(string movTitle, string movCategory)//These paramaters are settings provided
+        {   //property name should always be on the left side
+            title = movTitle;
+            category = movCategory;
+        }
+        #endregion
+
+        #region PrintInfo
+        public void PrintInfo()
+        {
+            
+            Console.WriteLine(Title);
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11_Movie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movies> MovieList = new List<Movies>();
+
+            MovieList.Add(new Movies("The Matrix", "Sci-Fi"));
+            MovieList.Add(new Movies("When Harry Met Sally", "Romance"));
+            MovieList.Add(new Movies("Bad Boys", "Action"));
+            MovieList.Add(new Movies("Bone Collector", "Crime"));
+            MovieList.Add(new Movies("Coming To America", "Comedy"));
+            MovieList.Add(new Movies("The Color Purple", "Drama"));
+            MovieList.Add(new Movies("The Notebook", "Romance"));
+            MovieList.Add(new Movies("Sugar Hill", "Crime"));
+            MovieList.Add(new Movies("Up", "Animation"));
+            MovieList.Add(new Movies("Despicable Me", "Animation"));
+
+
+            bool repeat = true;
+            while (repeat)
+            {
+                Console.Write("What movie category are you interested in? ");
+                string category = Console.ReadLine();
+
+                foreach (Movies m in MovieList)
+                {
+                    if (category.ToLower() == m.Category.ToLower())
+                    {
+                        m.PrintInfo();
+                    }
+
+                }
+
+                Console.Write("Would you like to select another category?(y/n)");
+                string input = Console.ReadLine().ToLower();
+
+
+                if (input == "y")
+                {
+                    repeat = true;
+                }
+
+                else
+                {
+                    Console.WriteLine("Goodbye!");
+                    break;
+                }
+               
+        
+
+
+                
+                
+
+            }
+
+
+
+        }
+    }
+}
